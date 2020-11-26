27c02acfbae537ae8c30669daa1f7b2a00759770
SanpuiRonak <ronaksanpui@gmail.com>
2020-11-27 00:06:56 +0530

    progress commit

--- src/commands/repo.js
+++ src/commands/repo.js
@@ -1,3 +1,7 @@
+const fetch = require("node-fetch");
+
+const deb = require("debug")("Repo Debug");
+
 function execute(msg, args) {
   if (args.length == 0) {
     msg.reply("Will list current repo");
@@ -7,9 +11,18 @@ function execute(msg, args) {
       if (args.length <= 1) {
         msg.reply("Link Pliz 😶");
         break;
-      }
-      if (validate(args[1]) === false) {
+      } else if (validate(args[1]) === false) {
         msg.reply(" the link seems to be broken💔");
+        deb(args[1]);
+        break;
+      }
+      const api = generateApi(args[1]);
+      let res = apiValdate(api);
+      console.log(res);
+
+      if (apiValdate(api) === false) {
+        msg.reply(" the link seems to be broken💔");
+        // break;
       } else {
         msg.reply("Adding");
       }
@@ -31,6 +44,24 @@ function execute(msg, args) {
 function validate(link) {
   let linkValidater = new RegExp("^https?://github.com/.+/.+$");
   return linkValidater.test(link);
+}
+
+function apiValdate(api) {
+  // deb(api);
+  fetch(api)
+    .then((res) => res.status)
+    .then((status) => {
+      deb(status);
+      if (status === 200) return true;
+      else return false;
+    });
+}
+
+function generateApi(link) {
+  let api = "https://api.github.com/repos";
+  let secondLastIndex = link.lastIndexOf("/", link.lastIndexOf("/") - 1);
+  api += link.substr(secondLastIndex);
+  return api;
 }
 
 module.exports = {
