332751bedaf2a0b925a772119b67c404db344dcd
Ronak Sanpui <ronaksanpui@gmail.com>
2021-04-21 14:57:35 +0530

    didi some renaming

--- src/database/schema/issueSchema.js
+++ src/database/schema/issueSchema.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const issueSchema = new mongoose.Schema({
   latestOpen: Number,
   latestClosed: Number,
+  issueChanel: Number,
 });
 
 module.exports = { issueSchema };
--- src/database/schema/repoSchema.js
+++ src/database/schema/repoSchema.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-const { commitSchema } = require("./commit");
-const { issueSchema } = require("./issue");
+const { commitSchema } = require("./commitSchema");
+const { issueSchema } = require("./issueSchema");
 
 const repoSchema = new mongoose.Schema({
   repoName: String,
