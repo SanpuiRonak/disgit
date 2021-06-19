c9b6c5245ef6068c72f9e5fb6a56fad63a545028
SanpuiRonak <ronaksanpui@gmail.com>
2021-06-19 21:20:07 +0530

    cleaned unneccesary schema

--- src/database/schema/commitSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const commitSchema = new mongoose.Schema({
-  channelId: Number,
-  // apiUrl: String,
-  commitCount: Number,
-});
-
-module.exports = { commitSchema };
--- src/database/schema/issueSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const issueSchema = new mongoose.Schema({
-  latestOpen: Number,
-  latestClosed: Number,
-  issueChanel: Number,    
-});
-
-module.exports = { issueSchema };
