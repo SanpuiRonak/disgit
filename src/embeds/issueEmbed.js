704af27d2532af1e0266ae5cddd2c5af6f103873
SanpuiRonak <ronaksanpui@gmail.com>
2021-07-16 18:53:03 +0530

    made emebed

--- src/embeds/issueEmbed.js
+++ src/embeds/issueEmbed.js
@@ -1,29 +1,27 @@
 const Discord = require("discord.js");
 
-// const { sendEmbed } = require("../main");
-
 function getEmbed(issueObj) {
+  let body;
+  if (issueObj.payload.issue.body.length > 100) {
+    body = issueObj.payload.issue.body.substring(0, 100) + "...";
+  }
+  else {
+    body = issueObj.payload.issue.body;
+  }
   const exampleEmbed = new Discord.MessageEmbed()
-    .setColor("#0099ff")
-    .setTitle("Some title")
-    .setURL("https://discord.js.org/")
+    .setColor("#FFFF00")
+    .setTitle(issueObj.payload.issue.title)
+    .setURL(issueObj.payload.issue.html_url)
     .setAuthor(
-      "Some name",
-      "https://i.imgur.com/wSTFkRM.png",
-      "https://discord.js.org"
+      issueObj.repo.name,
+      "",
+      "https://github.com/" + issueObj.repo.name
     )
-    .setDescription("Some description here")
-    .setThumbnail("https://i.imgur.com/wSTFkRM.png")
-    .addFields(
-      { name: "Regular field title", value: "Some value here" },
-      { name: "\u200B", value: "\u200B" },
-      { name: "Inline field title", value: "Some value here", inline: true },
-      { name: "Inline field title", value: "Some value here", inline: true }
-    )
-    .addField("Inline field title", "Some value here", true)
-    .setImage("https://i.imgur.com/wSTFkRM.png")
+    .setDescription(body)
+    // .setThumbnail("https://opengraph.githubassets.com/13e8e9da3d59baf06e01f5937109ed0b447faec0e5f6158735719cd50099ccbf/freeCodeCamp/devdocs")
+    .setThumbnail(issueObj.payload.issue.user.avatar_url)
     .setTimestamp()
-    .setFooter("Some footer text here", "https://i.imgur.com/wSTFkRM.png");
+    .setFooter(issueObj.actor.display_login, issueObj.payload.issue.user.avatar_url);
   return exampleEmbed;
 }
 
